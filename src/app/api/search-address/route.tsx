8941e6defbdeb734821e807908a6c91eecac44e1
CodeWithPassion000 <shivakumararadyasm5@gmail.com>
2024-03-16 08:03:22 +0530

    api issue resolved temparary

--- src/app/api/search-address/route.tsx
+++ src/app/api/search-address/route.tsx
@@ -1,18 +1,20 @@
 import { NextResponse } from 'next/server';
 
 const BASE_URL = 'https://api.mapbox.com/search/searchbox/v1/suggest';
+const MAPBOX_ACCESS_TOKEN =
+  'pk.eyJ1IjoiY29kZXdpdGhwYXNzaW9uMDAwIiwiYSI6ImNsdHNwMzE3aTB3cTYycXFtM3l1cnVsYzUifQ.q362IhUsHmCfLirKDp8z_A';
 
 interface Request {
   url: string;
 }
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
+export async function GET(request: string) {
+  // const { searchParams } = new URL(request.url);
 
-  const searchText = searchParams.get('q');
+  // const searchText = searchParams.get('q');
 
   const response = await fetch(
-    `${BASE_URL}?q=${searchText}?language=en&limit=8&session_token=[GENERATED-UUID]&proximity=-83.748708,42.265837&country=IN&access_token=${process.env.MAPBOX_ACCESS_TOKEN}`,
+    `${BASE_URL}?q=${request}?language=en&limit=8&session_token=[GENERATED-UUID]&proximity=-83.748708,42.265837&country=IN&access_token=${MAPBOX_ACCESS_TOKEN}`,
     {
       headers: {
         'Content-Type': 'application/json',
--- src/components/Booking/AutocompleteAddress.tsx
+++ src/components/Booking/AutocompleteAddress.tsx
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { GET } from '@/app/api/search-address/route';
+import { Response } from './auto-complete-response';
 
 function AutocompleteAddress() {
+  const [source, setSource] = useState<string>('');
+  const [addressList, setAddressList] = useState<Response | null>(null);
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      fetchAddresses();
+    }, 1000);
+
+    return () => {
+      clearTimeout(delayDebounceFn);
+    };
+  }, [source]);
+  const fetchAddresses = async () => {
+    try {
+      if (source) {
+        alert(source);
+        // const url = `/search?q=${encodeURIComponent(source)}`; // Construct the URL here
+        const response = await GET(source);
+        const data = await response.json();
+        setAddressList(data);
+      }
+    } catch (error) {
+      console.error('Error fetching addresses:', error);
+    }
+  };
+
   return (
     <div className="mt-5">
       <div>
@@ -10,9 +38,17 @@ function AutocompleteAddress() {
           className="bg-white p-1 
           border-[1px] w-full 
           rounded-md outline-none
-          focus:border-yellow-300
-          "
+          focus:border-yellow-300"
+          value={source}
+          onChange={(event) => {
+            setSource(event.target.value);
+          }}
         />
+        <div>
+          {addressList?.suggestions?.map((item, index) => (
+            <h2 key={index}>{item.place_formatted}</h2>
+          ))}
+        </div>
       </div>
       <div className="mt-3">
         <label className="text-gray-400">Where To?</label>
@@ -21,8 +57,7 @@ function AutocompleteAddress() {
           className="bg-white p-1 
           border-[1px] w-full 
           rounded-md outline-none
-          focus:border-yellow-300
-          "
+          focus:border-yellow-300"
         />
       </div>
     </div>
